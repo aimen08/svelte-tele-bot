49d924d55b01f676ceb88f8ed9f89a3403ae8c14
aimen08 <aimen_hamza@hotmail.com>
2023-05-15 20:27:04 +0100

    add types to telegram

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,16 @@
   import svelteLogo from './assets/svelte.svg'
   import viteLogo from '/vite.svg'
   import Counter from './lib/Counter.svelte'
+    let md;
+  	const initializeTelegram = () => {
+		md = window.Telegram.WebApp.themeParams ;
+    console.log(md);
+	}
 </script>
+
+<svelte:head>
+	<script src="https://telegram.org/js/telegram-web-app.js?1" on:load={initializeTelegram}></script>
+</svelte:head>
 
 <main>
   <div>
@@ -18,14 +27,6 @@
   <div class="card">
     <Counter />
   </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
 
 <style>
@@ -40,8 +41,5 @@
   }
   .logo.svelte:hover {
     filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
   }
 </style>
